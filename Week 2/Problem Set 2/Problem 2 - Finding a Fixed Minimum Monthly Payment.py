8bccb45d49900092b704ace959c8af6ce4ade7c0
Osamah Sarraj <osamah.sarraj@gmail.com>
2019-09-18 15:09:07 -0400

    deleting the unwanted lines

--- Week 2/Problem Set 2/Problem 2 - Finding a Fixed Minimum Monthly Payment.py
+++ Week 2/Problem Set 2/Problem 2 - Finding a Fixed Minimum Monthly Payment.py
@@ -6,16 +6,14 @@
 # balance = 84; annualInterestRate = 0.25
 #
 #Test Case 3(wrong): (290)
-balance = 3208; annualInterestRate = 0.15
+# balance = 3208; annualInterestRate = 0.15
 
 
 
 def finding_the_minimum_payment(balance, annualInterestRate):
-    # annualInterestRate = 0.2
     monthlyPaymentRate = annualInterestRate/12
     temp_balance = balance
     minimum_payment = 0
-    unpaid_balance = balance
 
     while balance >= 0:
         balance = temp_balance
