034bc89a976da89921c9dd2d293fa75b54ad7242
Osamah Sarraj <osamah.sarraj@gmail.com>
2019-09-18 15:09:54 -0400

    deleting the unwanted code (adding method)

--- Week 2/Problem Set 2/Problem 2 - Finding a more accurate Fixed Minimum Monthly Payment.py
+++ Week 2/Problem Set 2/Problem 2 - Finding a more accurate Fixed Minimum Monthly Payment.py
@@ -1,33 +1,31 @@
 
-#Test Case 1: (70)
-# balance = 757;annualInterestRate = 0.25
+# Test Case 1: (29157.09)
+# balance = 320000;annualInterestRate = 0.2
 
-# Test Case 2(correct): (10)
-# balance = 84; annualInterestRate = 0.25
-#
+# Test Case 1: (90325.03)
+# balance = 999999;annualInterestRate = 0.18
+
 #Test Case 3(wrong): (290)
-balance = 3208; annualInterestRate = 0.15
+# balance = 3208; annualInterestRate = 0.15
 
 
 
 def finding_the_minimum_payment(balance, annualInterestRate):
-    # annualInterestRate = 0.2
     monthlyPaymentRate = annualInterestRate/12
     temp_balance = balance
     minimum_payment = 0
-    unpaid_balance = balance
 
     while balance >= 0:
+        if balance >= (minimum_payment+10)*12:
+            minimum_payment += 10
+        else:
+            minimum_payment += 0.01
         balance = temp_balance
-        minimum_payment += 10
         for month in range(1, 13):
             unpaid_balance = balance - minimum_payment
             balance = unpaid_balance + monthlyPaymentRate * unpaid_balance
-
-
-
     return (minimum_payment)
 
 minimum_payment = finding_the_minimum_payment(balance,annualInterestRate)
 
-print ("Lowest Payment: " + str(minimum_payment) )
+print ("Lowest Payment: " + str("%.2f" % minimum_payment) )
