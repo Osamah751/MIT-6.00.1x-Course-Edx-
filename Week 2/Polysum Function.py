f414d107d3f37c70b5ddf7270ede8a13c21261d8
Osamah Sarraj <osamah.sarraj@gmail.com>
2019-09-29 19:05:47 -0400

    adding a description of how the function works

--- Week 2/Polysum Function.py
+++ Week 2/Polysum Function.py
@@ -9,6 +9,11 @@ Write a function called polysum that takes 2 arguments, n and s. This function s
 import math
 
 def polysum(n, s):
+    '''
+    :argument1 "n": is the number of sides
+    :argument2 "s": is the length of each side
+    :return: the sum the area and square of the perimeter of the polygon (rounded to 4 decimal places)
+    '''
     area = (0.25 * n * s**2) / (math.tan(math.pi / n) )
     perimeter_of_polysum = (n * s) ** 2
     total = round(area + perimeter_of_polysum, 4)
